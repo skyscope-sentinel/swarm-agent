8962283f22190c02e4876f7c9b1f436864eeb87f
root <michterter@proton.me>
2024-01-20 09:00:00 +0200

    🎉 feat: Add core application entry points

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import "./config/setup";
+
+import "./test";
+
+import { serve } from '@hono/node-server'
+import { isMainThread } from 'worker_threads';
+import { serveStatic } from '@hono/node-server/serve-static'
+
+import "./routes/session";
+import "./routes/complete";
+
+import { app, injectWebSocket } from './config/app';
+import { ioc } from './services';
+
+import { CC_EXECUTE_TEST, CC_WWWROOT_PORT } from "./config/params";
+
+app.use('/*', serveStatic({ root: './public' }))
+
+const main = () => {
+  if (!isMainThread) {
+    return;
+  }
+
+  if (CC_WWWROOT_PORT === -1) {
+    return;
+  }
+
+  if (CC_EXECUTE_TEST) {
+    return;
+  }
+
+  const server = serve({
+    fetch: app.fetch,
+    port: CC_WWWROOT_PORT,
+  });
+  
+  server.addListener("listening", () => {
+    console.log("Server listening on http://local.dev:80");
+  });
+  
+  injectWebSocket(server)  
+};
+
+main();
+
+ioc.loggerService.setDebug(!CC_EXECUTE_TEST);
